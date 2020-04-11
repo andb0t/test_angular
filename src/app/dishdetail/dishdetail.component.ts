cafa4c00beb8d6c770491a46da266429e5d8d338
Andreas Alexander Maier <andb0t@pm.me>
2020-04-11 15:42:32 +0200

    Use router parameters in observables for prev/next navigation

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
@@ -12,15 +14,26 @@ import { DishService } from '../services/dish.service';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(private dishService: DishService,
               private location: Location,
               private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    // tslint:disable-next-line:no-string-literal
-    const id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id).subscribe(dish => this.dish = dish);
+  ngOnInit() {
+    this.dishService.getDishIds()
+      .subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack() {
