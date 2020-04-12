d348531ec663e8a94674420aa243c8a8768737a6
Andreas Alexander Maier <andb0t@pm.me>
2020-04-12 11:17:22 +0200

    Refactor animations

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,7 +3,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility } from '../animations/app.animation';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
@@ -14,19 +14,7 @@ import { Comment } from '../shared/comment';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
-  animations: [
-  trigger('visibility', [
-      state('shown', style({
-          transform: 'scale(1.0)',
-          opacity: 1
-      })),
-      state('hidden', style({
-          transform: 'scale(0.5)',
-          opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
-  ]
+  animations: [ visibility() ]
 })
 export class DishdetailComponent implements OnInit {
 
